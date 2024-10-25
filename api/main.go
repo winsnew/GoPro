77717ed2b345014b4c9061d5f4619994b736193c
lossany <lossany08@gmail.com>
2024-10-25 16:24:38 +0700

    update directory

--- api/main.go
+++ api/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"goporto/internal/config"
+	"goporto/internal/http/handlers/home"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
 
 func main() {
-	fmt.Println("welcome")
+	cfg := config.LoadConfig()
+
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /api/hero", home.New())
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+
+	slog.Info("server started", slog.String("Address", cfg.Addr))
+	fmt.Printf("server started %s", cfg.Addr)
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %s", err.Error())
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutdown the server")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("failed to shutdown server", slog.String("error", err.Error()))
+	}
+	slog.Info("server shutdown success")
 }
